b2001b43af0d5cbae4b84d09f966360e2ef664af
Tan192005 <nathaniel.brizuela@jmc.edu.ph>
2025-03-08 15:11:15 +0800

    Update script (3).js

--- script (3).js
+++ script (3).js
@@ -1,7 +1,47 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
-}
-showTime();
-setInterval(function () {
-	showTime();
-}, 1000);
+<html>
+   <head>
+     <title>Teodoro warns allies will fight if China restricts flights over South China Sea</title>
+       <h1 id= "top">Teodoro warns allies will fight if China restricts flights over South China Sea</h1>
+       <h4>By: Gilberto Teodoro</h4>
+   </head>  
+   </body
+       <hr>
+       <img src="image.jpg" alt="" width="300px">
+       </p>
+       MANILA, Philippines (AP) — The Philippines and its security allies will take measures to counter any attempt by China to impose an air defense zone or restrict freedom of flights over the South China Sea.
+       <br>
+       <br>
+       Teodoro told The Associated Press in an interview that China’s increasing aggression in the disputed waters is now considered the greatest threat to the national security of the Philippines.
+       <br>
+       <br>
+       He believes it should also be regarded as a global threat because it can choke a key trade route crucial for global supply chains.
+       <br>
+       <br>
+       “The greatest external threat actually is Chinese aggression, Chinese expansionism and the attempt by China to change the international law through the use of force or acquiescence…or its attempt to reshape the world order to one that it controls,” Teodoro said.
+       <br>
+       <br>
+       There was no immediate reaction from Chinese officials.
+ Confrontations over the strategic waterway, which Beijing claimsalmost in its entirety, have spiked between Chinese and Philippine coast guard and navy forces in the last two years.
+       <br>
+       <br>
+       The long-seething territorial standoffs over the hotly-disputed shoals also involve Vietnam, Malaysia, Brunei and Taiwan.
+       <br>
+       <br>
+       Recent brushes involving Chinese military aircraft firing warning flares or flying dangerously close to drive away U.S., Australian and Philippine patrol aircraft have set off fresh alarms.
+       <br>
+       <br>
+       Teodoro said defense officials have discussed the possibility of China imposing a so-called Air Defense Identification Zone or any exclusion zone to restrict foreign aircraft movement in what Beijing regards as its airspace over the South China Sea.
+       <br>
+       <br>
+       That is a very serious transgression of international law, which will demand our response,” said Teodoro, a United States-educated lawyer and a licensed commercial pilot.
+       <br>
+       <br>
+       The Philippines will take a combination of measures singularly and with like-minded nations to counteract any such action by China, he said.
+       <br>
+       <br>
+       We have formulated contingency measures to respond or to have proper courses of action.”
+       </p>
+       <hr>
+       <a href="#top">back To top</a>
+       </body>
+       </html>
